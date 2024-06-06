0d7b1f1da65debc8261e51504aa5962884810c4c
jishnu Rahul <jishnurahulkanisetty@gmail.com>
2024-06-06 12:26:58 -0400

    Created

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Run your custom management command
-python manage.py generate_teams_grid
-
 # Apply database migrations
 python manage.py migrate
 
+# Run your custom management command
+python manage.py generate_teams_grid
 
 # Start the server
 gunicorn backend.wsgi  --log-file -
